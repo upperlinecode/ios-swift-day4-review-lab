8597c3b24971c51b39005d011ba5e318d3577d17
carsonlevine <carsonglevine@me.com>
2016-07-08 15:57:07 -0400

    first commit

---
+++ DayFourReview.playground/Contents.swift
@@ -0,0 +1,86 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//During this exercise, build on this Course class. Each additional task will be adding to the same class.
+
+class Course {
+    
+}
+
+//1. Give the class four properties: teacher (String), roster ([String]), name (String), and description (String). Give it an init method that initializes each instance with values for each property.
+
+//uncomment the lines below to test the init method
+//let english = Course(teacher: "Mr. Morris", roster: ["Doug Culcher", "Luke Arickson", "Kate Gebsun", "Albert Vang", "Randall Potter", "Leia Sestina", "Kevin Hubris", "Douglas Healey"], name: "Honors English 1", description: "A literature-focused course covering topics such as Shakespeare, Homer, Steinbeck, Orwell, and Austen.")
+//english.teacher //returns "Mr. Morris"
+//english.roster //returns ["Doug Culcher", "Luke Arickson", "Kate Gebsun", "Albert Vang", "Randall Potter", "Leia Sestina", "Kevin Hubris", "Douglas Healey"]
+//english.name //returns "Honors English 1"
+//english.description //returns "A literature-focused course covering topics such as Shakespeare, Homer, Steinbeck, Orwell, and Austen."
+
+
+
+
+
+
+
+
+//2. Add an instance method called addStudent to the Course class. Give it one parameter, student: String. The method should add that student to the roster.
+
+//uncomment the lines below to test the init method
+//english.addStudent("James Taylor")
+//english.roster //returns ["Doug Culcher", "Luke Arickson", "Kate Gebsun", "Albert Vang", "Randall Potter", "Leia Sestina", "Kevin Hubris", "Douglas Healey", "James Taylor"]
+
+
+
+
+
+
+
+
+//3. Add an instance method called removeStudent to the Course class. Give it one parameter, student: String. The method should remove that student from the roster.
+
+//uncomment the lines below to test the init method
+//english.removeStudent("Randall Potter")
+//english.roster //returns ["Doug Culcher", "Luke Arickson", "Kate Gebsun", "Albert Vang", "Randall Potter", "Leia Sestina", "Kevin Hubris", "Douglas Healey", "James Taylor"]
+
+
+
+
+
+
+
+
+//4. Add an instance method called printDescription to the Course class. The method should print a multiline description of the course, displaying its teacher, name, description, and complete roseter.
+
+//uncomment the line below to test the init method
+//english.printDescription()
+//prints:
+//Teacher: Mr. Morris
+//Course: Honors English 1
+//Description: A literature-focused course covering topics such as Shakespeare, Homer, Steinbeck, Orwell, and Austen.
+//Roster:
+//1. Doug Culcher
+//2. Luke Arickson
+//3. Kate Gebsun
+//4. Albert Vang
+//5. Leia Sestina
+//6. Kevin Hubris
+//7. Douglas Healey
+//8. James Taylor
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
